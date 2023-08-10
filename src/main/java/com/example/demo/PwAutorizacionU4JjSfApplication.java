b38ccf89a4e59c5de27429ef3405985385b57069
sdavdf <davidfueltala90@hotmail.com>
2023-08-09 19:47:04 -0500

    Taller 28

---
+++ src/main/java/com/example/demo/PwAutorizacionU4JjSfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4JjSfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4JjSfApplication.class, args);
+	}
+
+}
